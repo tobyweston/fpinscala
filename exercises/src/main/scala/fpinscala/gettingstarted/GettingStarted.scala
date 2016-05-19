f1873f4fda39a58b7d4f0eedaec70c0e38059a65
Toby Weston <toby.weston@gmail.com>
2016-05-19 06:46:16 +0100

    tidy

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -1,9 +1,7 @@
 package fpinscala.gettingstarted
 
-// A comment!
-/* Another comment */
-/** A documentation comment */
 object MyModule {
+
   def abs(n: Int): Int =
     if (n < 0) -n
     else n
@@ -13,8 +11,7 @@ object MyModule {
     msg.format(x, abs(x))
   }
 
-  def main(args: Array[String]): Unit =
-    println(formatAbs(-42))
+  def main(args: Array[String]): Unit = println(formatAbs(-42))
 
   // A definition of factorial, using a local, tail recursive function
   def factorial(n: Int): Int = {
@@ -42,8 +39,7 @@ object MyModule {
     msg.format(n, factorial(n))
   }
 
-  // We can generalize `formatAbs` and `formatFactorial` to
-  // accept a _function_ as a parameter
+  // We can generalize `formatAbs` and `formatFactorial` to accept a _function_ as a parameter
   def formatResult(name: String, n: Int, f: Int => Int) = {
     val msg = "The %s of %d is %d."
     msg.format(name, n, f(n))
@@ -54,16 +50,14 @@ object FormatAbsAndFactorial {
 
   import MyModule._
 
-  // Now we can use our general `formatResult` function
-  // with both `abs` and `factorial`
+  // Now we can use our general `forÔmatResult` function with both `abs` and `factorial`
   def main(args: Array[String]): Unit = {
     println(formatResult("absolute value", -42, abs))
     println(formatResult("factorial", 7, factorial))
   }
 }
 
-// Functions get passed around so often in FP that it's
-// convenient to have syntax for constructing a function
+// Functions get passed around so often in FP that it's convenient to have syntax for constructing a function
 // *without* having to give it a name
 object AnonymousFunctions {
 
@@ -85,11 +79,9 @@ object AnonymousFunctions {
 
 object MonomorphicBinarySearch {
 
-  // First, a binary search implementation, specialized to `Double`,
-  // another primitive type in Scala, representing 64-bit floating
-  // point numbers
-  // Ideally, we could generalize this to work for any `Array` type,
-  // so long as we have some way of comparing elements of the `Array`
+  // First, a binary search implementation, specialized to `Double`, another primitive type in Scala, representing
+  // 64-bit floating point numbers Ideally, we could generalize this to work for any `Array` type, so long as we
+  // have some way of comparing elements of the `Array`
   def binarySearch(ds: Array[Double], key: Double): Int = {
     @annotation.tailrec
     def go(low: Int, mid: Int, high: Int): Int = {
@@ -110,8 +102,8 @@ object MonomorphicBinarySearch {
 
 object PolymorphicFunctions {
 
-  // Here's a polymorphic version of `binarySearch`, parameterized on
-  // a function for testing whether an `A` is greater than another `A`.
+  // Here's a polymorphic version of `binarySearch`, parameterized on a function for testing whether an `A` is
+  // greater than another `A`.
   def binarySearch[A](as: Array[A], key: A, gt: (A, A) => Boolean): Int = {
     @annotation.tailrec
     def go(low: Int, mid: Int, high: Int): Int = {
@@ -128,8 +120,10 @@ object PolymorphicFunctions {
     go(0, 0, as.length - 1)
   }
 
-  // Polymorphic functions are often so constrained by their type
-  // that they only have one implementation! Here's an example:
+  // Polymorphic functions are often so constrained by their type that they only have one implementation! This is called
+  // parametricity.
+
+  // Here's an example:
 
   def partial1[A, B, C](a: A, f: (A, B) => C): B => C = (b: B) => f(a, b)
 
