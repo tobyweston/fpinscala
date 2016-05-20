2c25322c6f5d5c0a68e7c1dd69a01faafa5f2925
Toby Weston <toby.weston@gmail.com>
2016-05-20 07:49:14 +0100

    bit of a rubbish implementation of isSorted (exercise 2)

--- exercises/src/main/scala/fpinscala/Exercise2.scala
+++ exercises/src/main/scala/fpinscala/Exercise2.scala
@@ -1,10 +1,51 @@
 package fpinscala
 
+import fpinscala.Exercise2._
+
 /**
   * See [[fpinscala.gettingstarted.MyModule]]
   */
 object Exercise2 {
-  // Exercise 2: Implement a polymorphic function to check whether
-  // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = ???
+  // Exercise 2: Implement a polymorphic function to check whether an `Array[A]` is sorted
+
+  def isSorted[A](array: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def recur(n: Int): Boolean = {
+      if (n == 1) true
+      else if (!ordered(array(n - 1), array(n))) false
+      else recur(n - 1)
+    }
+
+    recur(array.length - 1)
+  }
+
+  def isSortedBroken[A](array: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def recur(first: A, tail: Array[A]): Boolean = {
+      if (!ordered(first, tail.head)) false
+      else recur(tail.head, tail.tail)
+    }
+
+    recur(array.head, array.tail)
+  }
+
+  // inspired by this function
+  def findFirst[A](array: Array[A], p: A => Boolean): Int = {
+    @annotation.tailrec
+    def recur(n: Int): Int = {
+      if (n >= array.length) -1
+      else if (p(array(n))) n
+      else recur(n + 1)
+    }
+
+    recur(0)
+  }
+}
+
+object Exercise2Test extends App {
+  println(findFirst(Array(1, 2, 3, 2, 5), (a: Int) => a > 3) + " position")
+
+  assert(isSorted(Array(1, 2, 3, 4, 5), (a: Int, b: Int) => b > a))
+  assert(!isSorted(Array(1, 2, 3, 4, 5), (a: Int, b: Int) => b < a))
+  assert(!isSorted(Array(1, 3, 3, 4, 5), (a: Int, b: Int) => b > a))
 }
