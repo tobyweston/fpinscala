977bbfa4039a78b9f48686dd1f28792ee9c59a28
Toby Weston <toby.weston@gmail.com>
2016-05-25 06:44:26 +0100

    implement 'curry'

--- exercises/src/main/scala/fpinscala/Exercise3.scala
+++ exercises/src/main/scala/fpinscala/Exercise3.scala
@@ -1,5 +1,7 @@
 package fpinscala
 
+import fpinscala.Exercise3.curry
+
 /**
   * See [[fpinscala.gettingstarted.MyModule]]
   */
@@ -8,9 +10,19 @@ object Exercise3 {
 
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
-  def curry[A, B, C](f: (A, B) => C): A => (B => C) =
-    ???
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
+    (a: A) => (b: B) => f(a,b)
+  }
 
   // NB: The `Function2` trait has a `curried` method already
 
+}
+
+object Exercise3Test extends App {
+  val add: (Int, Int) => Int = (a: Int, b: Int) => a + b
+  assert(add(1,1) == 2)
+
+  val curried: (Int) => (Int) => Int = curry(add)
+  assert(curried(1)(1) == 2)
 }
